93c7995f15e53771d0f90f53e7f843ee855eca5d
Pavel Prchal <prchalp@gmail.com>
2020-10-07 20:33:16 +0200

    heavy development

--- metronome_control.js
+++ metronome_control.js
@@ -2,24 +2,50 @@
 // -------------------- MetronomeControl
 // --------------------
 class MetronomeControl extends BaseControl{
-    constructor(holderId, a, b) {
+    constructor(holderId, metroA, metroB, bpm) {
         super();
         this.r = Raphael(holderId, 640, 480);
-        this.A = a;
-        this.B = b;
+        this.A = metroA;
+        this.B = metroB;
+        this.running = false;
+        this.bpm = 40;
     }
 
     render() {
         this.update(this.A, this.B);
     }  
 
+    start(){
+
+    }
+
+    stop(){
+
+    }
+    
+    toggleStartStop(){
+        this.running = !this.running;
+        if(this.running){
+            this.stop();
+        }
+        else{
+            this.start();
+        }
+        return this.running;
+    }
+
+
     update(metroA, metroB){
+        this.A = metroA;
+        this.B = metroB;
         this.debug(`Updating metronome to: ${metroA}/${metroB}`);
 
         let r = this.r;
+        r.clear();
         let angle = 0;
 
-        while (angle < 360) {
+        let aStep = 360 / this.A;
+        for(let a=0; a < this.A; a++){
             var color = Raphael.getColor();
             (function (t, c) {
                 r.circle(320, 450, 20).attr({stroke: c, fill: c, transform: t, "fill-opacity": .4}).click(function () {
@@ -30,11 +56,13 @@ class MetronomeControl extends BaseControl{
                     this.animate({"fill-opacity": .4}, 500);
                 });
             })("r" + angle + " 320 240", color);
-            angle += 30;
+            angle += aStep;
         }
+
         Raphael.getColor.reset();
         var s = r.set();
-        s.push(r.path("M320,240c-50,100,50,110,0,190").attr({fill: "none", "stroke-width": 2}));
+        this.rafika = r.path("M320,240c-50,100,50,110,0,190").attr({fill: "none", "stroke-width": 2})
+        s.push(this.rafika);
         s.push(r.circle(320, 450, 20).attr({fill: "none", "stroke-width": 2}));
         s.push(r.circle(320, 240, 5).attr({fill: "none", "stroke-width": 10}));
         s.attr({stroke: Raphael.getColor()});
