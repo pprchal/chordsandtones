ba834f435280a199ff639ac9f6b92b3b94982b5c
Pavel Prchal <prchalp@gmail.com>
2020-12-24 11:12:43 +0100

    fixes

--- core/leaflet.mjs
+++ core/leaflet.mjs
@@ -36,22 +36,22 @@ export const DB = {
   ],
 
   tones: [
-    { name: "C",  octave: 3 }, 
-    { name: "C#", octave: 3 },
-    { name: "D",  octave: 3 },
-    { name: "D#", octave: 3 },
-    { name: "E",  octave: 3 },
-    { name: "F",  octave: 3 },
-    { name: "F#", octave: 3 },
-    { name: "G",  octave: 3 },
-    { name: "G#", octave: 3 },
-    { name: "A",  octave: 3 },
-    { name: "B",  octave: 3 },
-    { name: "H",  octave: 3 }
+    { name: "C"  }, 
+    { name: "C#" },
+    { name: "D"  },
+    { name: "D#" },
+    { name: "E"  },
+    { name: "F"  },
+    { name: "F#" },
+    { name: "G"  },
+    { name: "G#" },
+    { name: "A"  },
+    { name: "B"  },
+    { name: "H"  }
   ],
 
   intervals:[
-    { name: "základní tón" },
+    { name: "čistá prima" },
     { name: "malá sekunda" },
     { name: "velká sekunda" },
     { name: "malá tercie" },
@@ -63,8 +63,7 @@ export const DB = {
     { name: "velká sexta" },
     { name: "malá septima" },
     { name: "velká septima" },
-    { name: "čistá oktáva" },
-    { name: "???" }
+    { name: "čistá oktáva" }
   ],
 
   tunings : [ 
@@ -108,7 +107,7 @@ export const DB = {
           { name: "nádech",    type: "-", offsets: [2,   5,   4,   3,   3,   4,   2,   3,   3,   4  ], bend: +1 },
           // { name: "1/2",       type: "-", offsets: [1,   5,   4,   3,   NaN, 9,   NaN, NaN, NaN, NaN] },
           // { name: "1",         type: "-", offsets: [NaN, 5,   9,   NaN, NaN, NaN, NaN, NaN, NaN, NaN] },
-          // { name: "1 1/2",     type: "-", offsets: [NaN, NaN, 8,   NaN, NaN, NaN, NaN, NaN, NaN, NaN] }
+          { name: "1 1/2",     type: "-", offsets: [NaN, NaN, 8,   NaN, NaN, NaN, NaN, NaN, NaN, NaN] }
       ],
       octaves : { "C": 4, "G": 3  }
     }
--- core/mharp.mjs
+++ core/mharp.mjs
@@ -11,22 +11,29 @@ export class MHarp {
     }
 
     static generateRow(rootTone, templateRow){
-        return templateRow
-            .offsets
-            .reduce((tones, offset, n) => 
-                {
-                    if(n === 0) {
-                        tones[n] = MCore.shiftTone(rootTone, offset);
-                    }else{
-                        tones[n] = MCore.shiftTone(tones[n - 1], offset);
-                    }
-                    return tones;
-                },
-                Array(templateRow.offsets.length)
-            );
+        let tones = Array(templateRow.offsets.length)
+
+        let prevTone = undefined;
+        for (let i=0; i < templateRow.offsets.length; i++){
+            let offset = templateRow.offsets[i]
+            if(isNaN(offset)){
+                tones[i] = undefined
+            }else if(prevTone === undefined){
+                tones[i] = MCore.shiftTone(rootTone, offset)
+                prevTone = tones[i]
+            }else {
+                tones[i] = MCore.shiftTone(prevTone, offset)
+                prevTone = tones[i]
+            }
+        }
+
+        return tones
     }
 
     static bendTone(tone, templateRow){
+        if(tone.name == undefined){
+            console.debug('tt')
+        }
         let bend = templateRow.bend
         if(bend !== 0)
             bend = tone.name.endsWith('#') ? bend : 0
